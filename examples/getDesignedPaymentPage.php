b9f7aa5ba3cddb3415c5a9543bf46f023c9a3e4f
Yaron Uliel <5229872+yaronuliel@users.noreply.github.com>
2025-10-23 18:31:11 +0300

    Add example file for payment page with customization

---
+++ examples/getDesignedPaymentPage.php
@@ -0,0 +1,48 @@
+<?php
+
+use CreditGuard\Consts\CreditType;
+use CreditGuard\Consts\Currency;
+use CreditGuard\Consts\Language;
+use CreditGuard\Consts\MpiValidation;
+use CreditGuard\Consts\TransactionType;
+use CreditGuard\Requests\DoDealRedirectRequest;
+
+require __DIR__ . '/bootstrap.php';
+
+$doDealRedirectRequest = new DoDealRedirectRequest();
+
+$doDealRedirectRequest
+    ->setVersion('2000')
+    ->setLanguage(Language::HEBREW)
+    ->setTransactionType(TransactionType::DEBIT)
+    ->setCreditType(CreditType::PAYMENTS)
+    ->setMpiValidation(MpiValidation::J102)
+    ->setUniqueid('test-1-' . time())
+    ->setSuccessUrl($baseurl . '/returnFromRedirect.php?status=success')
+    ->setErrorUrl($baseurl . '/returnFromRedirect.php?status=error')
+    //->setCancelUrl($baseurl . '/returnFromRedirect.php?status=cancel')
+    ->setDescription('Test transaction')
+    ->setNumberOfPayments('1 2 3 4 5')
+    //->setPaymentsInterest('0 2000 3000 4000 5000')
+    ->setCurrency(Currency::ILS)
+    ->setPaymentPageCustomTexts([
+        'cg-form-title' => 'הזין את פרטי האשראי להלןניתן ל'
+    ])
+//    ->setPaymentPageKeepCreditCardDetails(false)
+//    ->setPaymentPageUiLang('eng')
+//    ->setPaymentPageAncestorURL("https://original.example.com")
+    ->setPaymentPageCssFromFile(__DIR__ . '/files/ui.css')
+    ->setTotal(1);
+
+
+/** @var \CreditGuard\Responses\DoDealRedirectResponse $response */
+$response = $cg->execute($doDealRedirectRequest);
+
+
+if(php_sapi_name() != 'cli') {
+    header('Location: ' . $response->getMpiHostedPageUrl());
+} else {
+    echo 'Redirect the customer to: ' . $response->getMpiHostedPageUrl() . PHP_EOL;
+}
+
+exit;
